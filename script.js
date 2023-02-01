a041684634439e5310948c947906afee1aa65520
AdayOrtega <aday.ortega@gmail.com>
2023-02-01 16:12:57 +0000

    Platform movement clompeted

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
 const initialText = document.querySelector('.start');
+let pos = 235
+var gameStarted = false 
 document.addEventListener('keyup', function(event){
     if (event.key === "g") initialText.parentNode.removeChild(initialText);
-    startGame();
+    if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') {
+        platform.direction = null
+    }
+    if (!gameStarted){
+        startGame();
+        gameStarted = true
+    }
+    
+    
 })
 
 function startGame(){
     
-    const platform = document.querySelector('.platform');
+    
     document.addEventListener('keydown', function(event){
-        console.log(platform.style.left)
-        if (event.key === 'ArrowRight'){
-            let left = platform.style.left;
-            left = parseInt(left.substring(0,left.indexOf('p')))
-            left += 50;
-            platform.style.left = left + "px";
-        }
-        if(event.key === 'ArrowLeft'){
 
+        if (event.key === 'ArrowRight'){
+            platform.direction = 1
+        }
+
+        if(event.key === 'ArrowLeft'){ 
+            platform.direction = -1
         }
     })
+
+    const timerId = setInterval(gameEngine, 50)
+    
 }
+
+function gameEngine () {
+    platform.move()
+}
+
+function Platform (){
+    var self = this
+    this.sprite = document.querySelector('.platform');
+    this.direction = 0
+    this.left = 250
+    this.width = 100
+    this.speed = 24
+    
+    this.move = function () {
+       
+        if (this.direction === -1 && this.left <= this.speed
+            || this.direction ===
+            1 && this.left >= 600 - this.width - this.speed)
+            this.direction = 0;
+
+        this.left += this.direction * this.speed
+        this.sprite.style.left = this.left + 'px'
+        
+
+    }
+}
+
+const platform = new Platform()
+
+funtion Ball ()
