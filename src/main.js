e5c0a8c097a89eec5c1403eb27a412c28f18f3b3
Cristian Rivero Martín <cristianjrm.dev@gmail.com>
2023-02-07 13:58:15 +0000

    Fixed audio loop

--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
+//Initializing objects
 const game = new Game();
 const ball = new Ball();
 const platform = new Platform();
 const blockCollectionInstance = new BlockCollection(480, 240, 2, 2, 60, 60);
+
+//We fill the blocks div with all the blocks we create
 const blockHTML = document.querySelector('.blocks');
 blockCollectionInstance.draw();
 blockHTML.innerHTML = blockCollectionInstance.generateBlockCollection();
 blockCollectionInstance.drawAllBlocks()
 
+// Texts that inform the player (victory, defear, life lost, game start, life and scores)
 const initialText = document.querySelector('.start');
 const lostLifeText = document.querySelector('.life-lost');
 const victoryText = document.querySelector('.victory');
 const gameOverText = document.querySelector('.game-over');
+const scoreHTML = document.querySelector('.score-text')
+const recordTextsHTML = document.querySelectorAll('.record-text')
+const livesHTML = document.querySelector('.lives-text')
 
+// We link JS with the audio tags
 const blockCollisionAudio = document.querySelector("#blockCollisionAudio")
 const gameOverAudio = document.querySelector("#gameOverAudio")
 const loseLifeAudio = document.querySelector('#loseLifeAudio');
 const victoryAudio = document.querySelector("#victoryAudio")
-const scoreHTML = document.querySelector('.score-text')
-const recordTextsHTML = document.querySelectorAll('.record-text')
-const livesHTML = document.querySelector('.lives-text')
 const boing1 = document.querySelector('#boing1');
 const boing2 = document.querySelector('#boing2');
 
@@ -29,7 +34,7 @@ document.addEventListener('keyup', function (event) {
     lostLifeText.style.display = "none"
     victoryText.style.display = "none"
     gameOverText.style.display = "none"
-    
+
     game.startGame();
     game.gameStatus = 1
   }
@@ -38,7 +43,9 @@ document.addEventListener('keyup', function (event) {
   }
 })
 
+// Firefox compatibility for looping audio
 document.getElementById('backgroundAudio').addEventListener('ended', function () {
   console.log("audio finished")
   this.currentTime = 0;
+  this.play();
 }, false);
